764540e2ac5a1ac762373947b6078c1b286910c8
Laxmikanta Roy <roylaxmikanta00@gmail.com>
2026-08-19 00:03:48 +0530

    Time: 3 ms (53.63%), Space: 20.6 MB (6.84%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map={}
+        for i in range(len(nums)):
+            if target-nums[i] in hash_map:
+                return [i,hash_map[target-nums[i]]]
+            hash_map[nums[i]]=i
