15801e782230e2917dac5151e8a5ee2736ce67f2
Laxmikanta Roy <roylaxmikanta00@gmail.com>
2026-09-01 18:45:14 +0530

    Time: 11 ms (13.82%), Space: 19.1 MB (98.51%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        hash_map={'I':1,'V': 5,'X' : 10,'L' : 50,'C' : 100,'D' : 500,'M' : 1000}
+        num=0
+        for i in range(len(s)):
+            if i+1<len(s) and hash_map[s[i]]<hash_map[s[i+1]]:
+                num-=hash_map[s[i]]
+            else:
+                num+=hash_map[s[i]]
+        return num
