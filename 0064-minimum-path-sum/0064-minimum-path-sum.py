8e3ef61c80594b96b598213ac5d66c6b0179db46
Laxmikanta Roy <roylaxmikanta00@gmail.com>
2026-09-02 21:45:03 +0530

    Time: 11 ms (55.48%), Space: 21.8 MB (34.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,16 +4,17 @@ class Solution:
         n=len(grid[0])
         dp=[[-1]*n for _ in range(m)]
         dp[0][0]=grid[0][0]
-        def solve(i,j):
-            if i==0 and j==0:
-                return dp[0][0]
-            if i<0 or j<0:
-                return float('inf')
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            up=solve(i-1,j)
-            left=solve(i,j-1)
-            dp[i][j]=grid[i][j]+min(up,left)
-            return dp[i][j]
-        return solve(m-1,n-1)
-        
+        for i in range(m):
+            for j in range(n):
+                if i==0 and j==0:
+                    continue
+                if i==0:
+                    up=float('inf')
+                else:
+                    up=dp[i-1][j]
+                if j==0:
+                    left=float('inf')
+                else:
+                    left=dp[i][j-1]
+                dp[i][j]=grid[i][j]+min(up,left)
+        return dp[m-1][n-1]
