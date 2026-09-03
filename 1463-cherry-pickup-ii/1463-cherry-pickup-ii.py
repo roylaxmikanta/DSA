9fa76949ea2cafaa0bd9390f54eca42e1a87fcc9
Laxmikanta Roy <roylaxmikanta00@gmail.com>
2026-09-04 03:32:10 +0530

    Time: 1685 ms (10.92%), Space: 27.2 MB (65.4%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -3,23 +3,24 @@ class Solution:
         n=len(grid)
         m=len(grid[0])
         dp=[[[-1 for _ in range(m)] for _ in range(m)] for _ in range(n)]
-        def solve(i,j1,j2):
-            if j1<0 or j1>m-1 or j2<0 or j2>m-1:
-                return float('-inf')
-            if i==n-1:
+        for j1 in range(m):
+            for j2 in range(m):
                 if j1==j2:
-                    return grid[i][j1]
-                return grid[i][j1]+grid[i][j2]
-            if dp[i][j1][j2]!=-1:
-                return dp[i][j1][j2]
-            maxi=0
-            for new_j1 in range(-1,2):
-                for new_j2 in range(-1,2):
-                    if j1==j2:
-                        ans=grid[i][j1]+solve(i+1,j1+new_j1,j2+new_j2)
-                    else:
-                        ans=grid[i][j1]+grid[i][j2]+solve(i+1,j1+new_j1,j2+new_j2)
-                    maxi=max(maxi,ans)
-            dp[i][j1][j2]=maxi
-            return dp[i][j1][j2]
-        return solve(0,0,m-1)
+                    dp[n-1][j1][j2]=grid[n-1][j1]
+                else:
+                    dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2]
+        for i in range(n-2,-1,-1):
+            for j1 in range(m):
+                for j2 in range(m):
+                    maxi=0
+                    for new_j1 in (-1,0,1):
+                        for new_j2 in (-1,0,1):
+                            if j1+new_j1<0 or j1+new_j1>=m or j2+new_j2<0 or j2+new_j2>=m:
+                                ans=float('-inf')
+                            elif j1==j2:
+                                ans=grid[i][j1]+dp[i+1][j1+new_j1][j2+new_j2]
+                            else:
+                                ans=grid[i][j1]+grid[i][j2]+dp[i+1][j1+new_j1][j2+new_j2]
+                            maxi=max(maxi,ans)
+                    dp[i][j1][j2]=maxi
+        return dp[0][0][m-1]
