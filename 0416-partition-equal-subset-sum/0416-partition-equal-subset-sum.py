051647b332985375fd2be9ddf18e8577f5942249
Laxmikanta Roy <roylaxmikanta00@gmail.com>
2026-09-05 17:51:03 +0530

    Time: 1151 ms (38.05%), Space: 19.4 MB (69.83%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,16 +4,18 @@ class Solution:
         if sumi%2!=0:
             return False
         target=sumi//2
-        dp=[[False]*(target+1) for _ in range(len(nums))]
-        for i in range(len(nums)):
-            dp[i][0]=True
+        prev=[False]*(target+1)
+        prev[0]=True
         if nums[0]<=target:
-            dp[0][nums[0]]=True
+            prev[nums[0]]=True
         for index in range(1,len(nums)):
+            curr=[False]*(target+1)
+            curr[0]=True
             for total in range(0,target+1):
                 pick=False
                 if nums[index]<=total:
-                    pick=dp[index-1][total-nums[index]]
-                not_pick=dp[index-1][total]
-                dp[index][total]=pick or not_pick
-        return dp[len(nums)-1][target]
+                    pick=prev[total-nums[index]]
+                not_pick=prev[total]
+                curr[total]=pick or not_pick
+            prev=curr
+        return prev[target]
